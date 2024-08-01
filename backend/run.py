43aa43b5dcbc73dffbd156c25815a82fb9465da8
Venkatesh Murthy <venki@life9sys.com>
2024-08-01 11:00:39 +0530

    changed gunicorn

--- backend/run.py
+++ backend/run.py
@@ -2,6 +2,7 @@
 from src import create_app
 from src.config import DevConfig, ProdConfig
 
+app = create_app(DevConfig)
+
 if __name__ == "__main__":
-    create_app(DevConfig).run(host="0.0.0.0",port = 5000)
-    # create_app(ProdConfig).run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
