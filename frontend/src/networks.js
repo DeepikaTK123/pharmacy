058c2fbd8db6f4a0b31618f8aebac4c37f4d7f14
Venkatesh Murthy <venki@life9sys.com>
2024-08-01 11:15:04 +0530

    build

--- frontend/src/networks.js
+++ frontend/src/networks.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const ip="https://128.199.19.234"
-// const ip ='http://' + window.location.hostname;
-// const ip="http://192.168.31.117"
-const BASE_URL = ip + ":5000";
-console.log(BASE_URL)
+// Use HTTPS base URL
+const BASE_URL = 'https://care4link.com/api';
+console.log(BASE_URL);
+
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
@@ -33,80 +32,81 @@ axiosInstance.interceptors.response.use((response) => {
 });
 
 export const login = (data) => {
-  return axiosInstance.post('/api/users/login', data);
+  return axiosInstance.post('/users/login', data);
 };
 
 export const getDashboardCount = () => {
-  return axiosInstance.get('/api/dashboard/getdashboardcount');
+  return axiosInstance.get('/dashboard/getdashboardcount');
 };
 
 export const getRevenueChart = (period) => {
-  return axiosInstance.post('/api/dashboard/getrevenuechart', { period });
+  return axiosInstance.post('/dashboard/getrevenuechart', { period });
 };
 
 export const getLowStockMedicines = () => {
-  return axiosInstance.get('/api/dashboard/getlowstockmedicines');
+  return axiosInstance.get('/dashboard/getlowstockmedicines');
 };
 
 // Pharmacy API
 export const addMedicine = (data) => {
-  return axiosInstance.post('/api/medicines/addmedicine', data);
+  return axiosInstance.post('/medicines/addmedicine', data);
 };
 
 export const getMedicines = () => {
-  return axiosInstance.get('/api/medicines/getmedicines');
+  return axiosInstance.get('/medicines/getmedicines');
 };
 
 export const updateMedicine = (data) => {
-  return axiosInstance.post('/api/medicines/edit', data);
+  return axiosInstance.post('/medicines/edit', data);
 };
 
 export const deleteMedicine = (data) => {
-  return axiosInstance.post('/api/medicines/delete', { id: data });
+  return axiosInstance.post('/medicines/delete', { id: data });
 };
 
 // Billing API
 export const addBillingRecord = (data) => {
-  return axiosInstance.post('/api/billing/add', data);
+  return axiosInstance.post('/billing/add', data);
 };
 
 export const getBillingRecords = () => {
-  return axiosInstance.get('/api/billing/get');
+  return axiosInstance.get('/billing/get');
 };
 
 export const updateBillingRecord = (data) => {
-  return axiosInstance.post('/api/billing/edit', data);
+  return axiosInstance.post('/billing/edit', data);
 };
 
 export const deleteBillingRecord = (id) => {
-  return axiosInstance.post('/api/billing/delete', { id });
+  return axiosInstance.post('/billing/delete', { id });
 };
 
 export const getPatientName = (phoneNumber) => {
-  return axiosInstance.post('/api/billing/getpatientname', { phone_number: phoneNumber });
+  return axiosInstance.post('/billing/getpatientname', { phone_number: phoneNumber });
 };
 
 export const getHighBillingUsers = () => {
-  return axiosInstance.get('/api/dashboard/gethighbillingusers');
+  return axiosInstance.get('/dashboard/gethighbillingusers');
 };
 
 export const addSupplier = (data) => {
-  return axiosInstance.post('/api/suppliers/add', data);
+  return axiosInstance.post('/suppliers/add', data);
 };
 
 export const getSuppliers = () => {
-  return axiosInstance.get('/api/suppliers/get');
+  return axiosInstance.get('/suppliers/get');
 };
 
 export const updateSupplier = (data) => {
-  return axiosInstance.post('/api/suppliers/edit', data);
+  return axiosInstance.post('/suppliers/edit', data);
 };
 
 export const deleteSupplier = (id) => {
-  return axiosInstance.post('/api/suppliers/delete', { id });
+  return axiosInstance.post('/suppliers/delete', { id });
 };
+
 export const uploadInvoice = (data) => {
-  return axiosInstance.post('/api/invoices/upload', data, {
+  return axiosInstance.post('/invoices/upload', data, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
